3da2606aa53359e0e782f95bf09bd620fe3a5cda
codefrec <codefrec@gmail.com>
2022-02-07 23:05:54 +0530

    brj

---
+++ controllers/AuthController.js
@@ -0,0 +1,284 @@
+var bodyParser = require("body-parser");
+var urlencodeParser = bodyParser.urlencoded({ extended: false });
+
+var axios = require("axios");
+var MockAdapter = require("axios-mock-adapter");
+
+// const LocalStorage = require('node-localstorage').LocalStorage,
+//   localStorage = new LocalStorage('./scratch');
+// const localStorage = require("localStorage");
+// This sets the mock adapter on the default instance
+//var mock = new MockAdapter(axios);
+
+const cookieParser = require("cookie-parser");
+
+let users = [
+  {
+    id: 1,
+    username: "admin",
+    password: "123456",
+    email: "admin@themesbrand.com",
+  },
+];
+
+// Mock GET request to /users when param `searchText` is 'John'
+// mock.onGet("/users", { params: { searchText: "John" } }).reply(200, {
+//   users: users,
+// });
+
+module.exports = function (app) {
+  app.use(cookieParser());
+  // Inner Auth
+  app.get("/auth-login", function (req, res) {
+    res.locals = { title: "Login" };
+    res.render("AuthInner/auth-login");
+  });
+  app.get("/auth-login-2", function (req, res) {
+    res.locals = { title: "Login 2" };
+    res.render("AuthInner/auth-login-2");
+  });
+  app.get("/auth-register", function (req, res) {
+    res.locals = { title: "Register" };
+    res.render("AuthInner/auth-register");
+  });
+  app.get("/auth-register-2", function (req, res) {
+    res.locals = { title: "Register 2" };
+    res.render("AuthInner/auth-register-2");
+  });
+  app.get("/auth-recoverpw", function (req, res) {
+    res.locals = { title: "Recoverpw" };
+    res.render("AuthInner/auth-recoverpw");
+  });
+  app.get("/auth-recoverpw-2", function (req, res) {
+    res.locals = { title: "Recoverpw 2" };
+    res.render("AuthInner/auth-recoverpw-2");
+  });
+  app.get("/auth-lock-screen", function (req, res) {
+    res.locals = { title: "Lock Screen" };
+    res.render("AuthInner/auth-lock-screen");
+  });
+  app.get("/auth-lock-screen-2", function (req, res) {
+    res.locals = { title: "Lock Screen 2" };
+    res.render("AuthInner/auth-lock-screen-2");
+  });
+  app.get("/auth-confirm-mail", function (req, res) {
+    res.locals = { title: "Confirm Mail" };
+    res.render("AuthInner/auth-confirm-mail");
+  });
+  app.get("/auth-confirm-mail-2", function (req, res) {
+    res.locals = { title: "Confirm Mail 2" };
+    res.render("AuthInner/auth-confirm-mail-2");
+  });
+  app.get("/auth-email-verification", function (req, res) {
+    res.locals = { title: "Email verification" };
+    res.render("AuthInner/auth-email-verification");
+  });
+  app.get("/auth-email-verification-2", function (req, res) {
+    res.locals = { title: "Email verification 2" };
+    res.render("AuthInner/auth-email-verification-2");
+  });
+  app.get("/auth-two-step-verification", function (req, res) {
+    res.locals = { title: "Two step verification" };
+    res.render("AuthInner/auth-two-step-verification");
+  });
+  app.get("/auth-two-step-verification-2", function (req, res) {
+    res.locals = { title: "Two step verification 2" };
+    res.render("AuthInner/auth-two-step-verification-2");
+  });
+
+  // Crypto
+  app.get("/crypto-ico-landing", function (req, res) {
+    res.locals = { title: "Crypto Ico Landing" };
+    res.render("Crypto/crypto-ico-landing");
+  });
+
+  // Auth Pages
+
+  app.get("/pages-maintenance", function (req, res) {
+    res.locals = { title: "Pages Maintenance" };
+    res.render("Pages/pages-maintenance");
+  });
+  app.get("/pages-comingsoon", function (req, res) {
+    res.locals = { title: "Pages Comingsoon" };
+    res.render("Pages/pages-comingsoon");
+  });
+
+  app.get("/pages-404", function (req, res) {
+    res.locals = { title: "Pages 404" };
+    res.render("Pages/pages-404");
+  });
+  app.get("/pages-500", function (req, res) {
+    res.locals = { title: "Pages 500" };
+    res.render("Pages/pages-500");
+  });
+
+  app.get("/register", function (req, res) {
+    if (req.user) {
+      res.redirect("Dashboard/index");
+    } else {
+      res.render("Auth/auth-register", {
+        message: req.flash("message"),
+        error: req.flash("error"),
+      });
+    }
+  });
+
+  app.post("/post-register", urlencodeParser, function (req, res) {
+    let tempUser = {
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    };
+    users.push(tempUser);
+
+    // Assign value in session
+    sess = req.session;
+    sess.user = tempUser;
+
+    res.redirect("/");
+  });
+
+  app.get("/login", function (req, res) {
+    res.render("Auth/auth-login", {
+      message: req.flash("message"),
+      error: req.flash("error"),
+    });
+  });
+
+  app.post("/post-login", urlencodeParser, async function (req, res) {
+    //mock.onPost("/post-login").reply(async function (config) {
+    // const validUser = users.filter(
+    //   (usr) =>
+    //     usr.email === req.body.email && usr.password === req.body.password
+    // );
+    // if (validUser["length"] === 1) {
+    //   // Assign value in session
+    sess = req.session;
+    // sess.user = validUser;
+    let body = {
+      email: req.body.email,
+      password: req.body.password,
+    };
+
+    //   try {
+    //     let url = `http://localhost:3000/api/v1/users/login`;
+    //     const response = await fetch(url, {
+    //       method: "POST",
+    //       body: JSON.stringify(body),
+    //     });
+
+    //     const data = await response.json();
+    //     console.log(data);
+    //     res.send("fffffffffffff");
+    //   } catch (error) {
+    //     console.log(error);
+    //   }
+    // });
+    try {
+      const response = await axios.post(
+        "http://localhost:3000/api/v1/users/login",
+        body
+      );
+
+      // localStorage.setItem("user", JSON.stringify(response.data));
+
+      // console.log(response.data);
+      res.cookie("user=" + JSON.stringify(response.data), expires = Date.now() + 99999999999999999999);
+      // console.log(response.data);
+      res.redirect("/");
+
+      //res.json("Hello");
+    } catch (ex) {
+
+      req.flash("error", "Incorrect email or password!");
+      res.redirect("/login");
+    }
+  });
+  // } else {
+  //   req.flash("error", "Incorrect email or password!");
+  //   res.redirect("/login");
+  // }
+  //  });
+
+  app.get("/forgot-password", function (req, res) {
+    res.render("Auth/auth-forgot-password", {
+      message: req.flash("message"),
+      error: req.flash("error"),
+    });
+  });
+
+  app.post("/post-forgot-password", urlencodeParser, async (req, res) => {
+    // const validUser = users.filter((usr) => usr.email === req.body.email);
+    // if (validUser["length"] === 1) {
+    //   req.flash("message", "We have e-mailed your password reset link!");
+    //   res.redirect("/forgot-password");
+    // } else {
+    //   req.flash("error", "Email Not Found !!");
+    //   res.redirect("/forgot-password");
+    // }
+    let body = {
+      email: req.body.email,
+    };
+    try {
+      const response = await axios.post(
+        "http://localhost:3000/api/v1/users/forget_password",
+        body
+      );
+      console.log(response.data);
+      req.flash("message", "We have e-mailed your password reset link!");
+      res.redirect("/reset-pass");
+      //res.json("Hello");
+    } catch (ex) {
+      req.flash("error", "Email Not Found !!");
+      res.redirect("/forgot-password");
+    }
+  });
+  app.get("/reset-pass", function (req, res) {
+    res.render("Auth/reset-pass", {
+      message: req.flash("message"),
+      error: req.flash("error"),
+    });
+  });
+  //post-reset-pass
+  app.post("/post-reset-pass", urlencodeParser, async (req, res) => {
+    let body = {
+      email: req.body.email,
+      otp: req.body.resetpass,
+      password: req.body.newpass,
+    };
+    try {
+      const response = await axios.post(
+        "http://localhost:3000/api/v1/users/verify_otp",
+        body
+      );
+      console.log(response.data);
+      if (response.status == 200) {
+        try {
+          const response = await axios.post(
+            "http://localhost:3000/api/v1/users/change_password",
+            body
+          );
+          console.log("hhhhhhhhhhhhhh");
+
+          req.flash("message", response.data);
+          res.redirect("/forgot-password");
+        } catch (ex) {
+          req.flash("error", "Email Not Found !!");
+          res.redirect("/forgot-password");
+        }
+      }
+
+      //res.json("Hello");
+    } catch (ex) {
+      req.flash("error", "Email Not Found !!");
+      res.redirect("/forgot-password");
+    }
+  });
+  app.get("/logout", function (req, res) {
+    // Assign  null value in session
+    sess = req.session;
+    sess.user = null;
+
+    res.redirect("/login");
+  });
+};
---
+++ public/assets/js/pages/dashboard.init.js
@@ -0,0 +1,159 @@
+setTimeout(function () {
+  $("#subscribeModal").modal("show");
+}, 2e3);
+var options = {
+  chart: {
+    height: 360,
+    type: "bar",
+    stacked: !0,
+    toolbar: { show: !1 },
+    zoom: { enabled: !0 },
+  },
+  plotOptions: {
+    bar: { horizontal: !1, columnWidth: "15%", endingShape: "rounded" },
+  },
+  dataLabels: { enabled: !1 },
+  series: [
+    {
+      name: "Series A",
+      data: [44, 55, 41, 67, 22, 43, 36, 52, 24, 18, 36, 48],
+    },
+    {
+      name: "Series B",
+      data: [13, 23, 20, 8, 13, 27, 18, 22, 10, 16, 24, 22],
+    },
+    {
+      name: "Series C",
+      data: [11, 17, 15, 15, 21, 14, 11, 18, 17, 12, 20, 18],
+    },
+  ],
+  xaxis: {
+    categories: [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ],
+  },
+  colors: ["#556ee6", "#f1b44c", "#34c38f"],
+  legend: { position: "bottom" },
+  fill: { opacity: 1 },
+},
+  chart = new ApexCharts(
+    document.querySelector("#stacked-column-chart"),
+    options
+  );
+chart.render();
+options = {
+  chart: { height: 200, type: "radialBar", offsetY: -10 },
+  plotOptions: {
+    radialBar: {
+      startAngle: -135,
+      endAngle: 135,
+      dataLabels: {
+        name: { fontSize: "13px", color: void 0, offsetY: 60 },
+        value: {
+          offsetY: 22,
+          fontSize: "16px",
+          color: void 0,
+          formatter: function (e) {
+            return e + "%";
+          },
+        },
+      },
+    },
+  },
+  colors: ["#556ee6"],
+  fill: {
+    type: "gradient",
+    gradient: {
+      shade: "dark",
+      shadeIntensity: 0.15,
+      inverseColors: !1,
+      opacityFrom: 1,
+      opacityTo: 1,
+      stops: [0, 50, 65, 91],
+    },
+  },
+  stroke: { dashArray: 4 },
+  series: [67],
+  labels: ["Series A"],
+};
+// var bodyParser = require("body-parser");
+// var urlencodeParser = bodyParser.urlencoded({ extended: false });
+// var axios = require("axios");
+async function readURL(input) {
+  var form = new FormData();
+
+  form.append("profile", document.getElementById("file-input").files[0]);
+
+  $.ajax({
+    method: "POST",
+    url: "http://localhost:3000/api/v1/users/edit_profile?id=61fd6f12fbdac1c720f8b3e7",
+    data: form,
+    contentType: false,
+    processData: false,
+    cache: false,
+    success: function (res) {
+      alert("Profile updated successfully");
+      console.log(res);
+      document.cookie += "profile"
+      document.getElementById("img").src =
+        `http://localhost:3000/profiles/${res.profile}`;
+      // window.location.href = redirect;
+    },
+    error: function (error) {
+      // $(".alert").css("display","block");
+      // $("#status").html(error.status+" ");
+      // $("#msg").html(error.responseText);
+      console.log(error);
+    },
+  });
+  //  var url = input.value;
+  //   var ext = url.substring(url.lastIndexOf(".") + 1).toLowerCase();
+  //   if (
+  //     input.files &&
+  //     input.files[0] &&
+  //     (ext == "gif" || ext == "png" || ext == "jpeg" || ext == "jpg")
+  //   ) {
+  //     var reader = new FileReader();
+
+  //     reader.onload = function (e) {
+  //       $("#img").attr("src", e.target.result);
+  //     };
+  //     reader.readAsDataURL(input.files[0]);
+  // var formData = new FormData();
+  // const profile = document.getElementById("file-input");
+  // formData.append("profile", profile.files[0]);
+  // try {
+  //   const response = await fetch(
+  //     "http://localhost:3000/api/v1/users/edit_profile?id=61fd6f12fbdac1c720f8b3e7",
+  //     {
+  //       body: formData,
+  //       headers: {
+  //         "Content-Type": "multipart/form-data",
+  //         boundary: "—-WebKitFormBoundaryfgtsKTYLsT7PNUVD",
+  //       },
+  //       method: "post",
+  //     }
+  //   );
+  //   console.log(response);
+  //   //  res.redirect("/");
+
+  //   //res.json("Hello");
+  // } catch (ex) {
+  //   console.log(ex);
+  //   // res.redirect("/");
+  // }
+  //   } else {
+  //     $("#img").attr("src", "/assets/no_preview.png");
+  //   }
+}
